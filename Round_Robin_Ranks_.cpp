f7dccb024180a530a2c25f752099209416f7a392
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2021-10-31 16:31:38 +0530

    cs improvement

--- Round_Robin_Ranks_.cpp
+++ Round_Robin_Ranks_.cpp
@@ -1,15 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void solve(){
+       long long n,k;
+        cin>>n>>k;
+        long long ans = ((2 * n) - k - 1)/2;
+        cout<<ans*2<<endl;
+}
+
 int main(){
     int t;
     cin>>t;
     while (t--)
     {
-        long long n,k;
-        cin>>n>>k;
-        long long ans = ((2 * n) - k - 1)/2;
-        cout<<ans*2<<endl;
+     solve();
     }
-    
 }
