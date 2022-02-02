9d263f46671d34ccb6d630b0940068b7606ae548
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-02-02 22:28:13 +0530

    changes in snippet

--- Java/CodeSnippet.java
+++ Java/CodeSnippet.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class CodeSnippet {
     public static void main(String args[]){
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            
+        }
     }
     
 }
