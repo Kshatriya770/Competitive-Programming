5cf5a39eabffef26b55998b19b24b76d85bc9e7a
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-02-04 15:31:40 +0530

    da program1

---
+++ C++/practical1.py
@@ -0,0 +1,13 @@
+import math
+
+arr = list(map(int,input().split()))
+print(arr)
+print("Maximum = ", max(arr))
+print("Minnimum = ", min(arr))
+print("Sum = ", sum(arr))
+avg = sum(arr)/len(arr)
+print("Average = ", avg)
+n = int(input("Enter the number for the square root of: "))
+print(math.sqrt(n))
+p = float(input("Enter the number for the round off: "))
+print(round(p,2))
