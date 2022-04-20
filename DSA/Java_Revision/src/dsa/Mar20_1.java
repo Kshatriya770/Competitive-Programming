d5cdc5d929400edabd2c8df44f93af2ce7ce402a
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-04-20 08:13:57 +0530

    Day 3

---
+++ DSA/Java_Revision/src/dsa/Mar20_1.java
@@ -0,0 +1,17 @@
+package dsa;
+import java.util.*;
+
+public class Mar20_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.nextLine();
+		String s = in.nextLine();
+		
+		char ch = s.charAt(n-1);
+		System.out.println(ch);
+	}
+
+}
---
+++ DSA/Java_Revision/src/dsa/Mar20_2.java
@@ -0,0 +1,17 @@
+
+package dsa;
+import java.util.*;
+
+public class Mar20_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		in.nextLine();
+		String T = in.nextLine();
+		
+
+	}
+
+}
