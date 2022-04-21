b4d7cc1b427828505a0f3d7b25ae219591d37f04
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-04-22 00:29:02 +0530

    Day 3

---
+++ DSA/Java_Revision/src/dsa/B.java
@@ -0,0 +1,20 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class B {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner in = new Scanner(System.in);
+        int v = in.nextInt();
+        int f = in.nextInt();
+        int m = in.nextInt();
+        int t = in.nextInt();
+
+        int add = f + m + t;
+        int div = v % add;
+        if(div )
+    }
+
+}
