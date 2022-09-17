23ac1211996774d20432ccd5ae240b3e6c43e735
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-09-17 15:12:28 +0530

    afd

---
+++ DSA/Java_Revision/src/dsa/Main.java
@@ -0,0 +1,18 @@
+package dsa;
+import java.util.Linkedlist;
+
+
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(21);
+
+        list.display();
+    }
+}
---
+++ DSA/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(21);
+
+        list.display();
+    }
+}
