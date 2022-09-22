6cbbcb97a2e602c5fc60341c7815544bf4d6ad45
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-09-22 23:37:46 +0530

    New Bin

---
+++ NewBeg/Starter/src/LinkedList.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+//Singly linkedlist
+public class LinkedList{
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+    public LinkedList(){
+        this.size = 0;
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        
+        head = node;
+
+        if(tail == null){
+            tail = head;
+        }
+        size += 1;
+    }
+
+    /**
+     * 
+     */
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+    }
+
+    private class Node{
+        public LinkedList.Node next;
+        public Object temp;
+        private int value;
+        // private Node next;
+
+        public Node(int value){
+            this.value = value;
+
+        }
+
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ NewBeg/Starter/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(21);
+
+        list.display();
+    }
+}
