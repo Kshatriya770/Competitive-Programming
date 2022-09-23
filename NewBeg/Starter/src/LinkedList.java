6fc79a51f5ab792d2bac212fa04848f84c4745de
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-09-23 23:46:22 +0530

    Deletion in LL

--- NewBeg/Starter/src/LinkedList.java
+++ NewBeg/Starter/src/LinkedList.java
@@ -9,6 +9,7 @@ public class LinkedList{
         this.size = 0;
     }
 
+    //Inserting elements at the first 
     public void insertFirst(int val){
         Node node = new Node(val);
         node.next = head;
@@ -21,6 +22,7 @@ public class LinkedList{
         size += 1;
     }
 
+    //Inserting elements at a given index
     public void insert(int val, int index){
         if(index == 0){
             insertFirst(val);
@@ -41,6 +43,7 @@ public class LinkedList{
         size++;
     }
 
+    //Inserting elements at Last
     public void insertLast(int val){
         if (tail == null) {
             insertFirst(val);
@@ -52,8 +55,66 @@ public class LinkedList{
         size++;
     }
 
+    public int deleteFirst(){
+        int val = head.value;
+        head = head.next;
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public int deleteLast(){
+        if(size <= 1){
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+
+        return val;
+    }
+
+    public Node get(int index){
+        Node node  = head;
+        for(int i = 0;i< index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+
+    public int delete(int index){
+        if(index == 0){
+            return deleteFirst();
+        }
+        if(index == size - 1){
+            return deleteLast();
+        }
+
+        Node prev = get(index -1);
+        int val = prev.next.value;
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+    //Find
+    public Node find(int value){
+        Node node = head; 
+        while(node != null){
+            if(node.value == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
     /**
-     * 
+     * display
      */
     public void display(){
         Node temp = head;
--- NewBeg/Starter/src/Main.java
+++ NewBeg/Starter/src/Main.java
@@ -12,6 +12,16 @@ public class Main {
         list.insertLast(88);
         list.insert(5, 3);
 
+        System.out.println(list.deleteFirst());
+
         list.display();
+        System.out.println();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println();
+        list.delete(2);
+        list.display();
+        list.find(2);
+        
     }
 }
