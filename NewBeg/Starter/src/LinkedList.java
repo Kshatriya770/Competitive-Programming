3a53aef084d316197f79e2e78a50ce4f1e88eb98
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-10-05 15:14:50 +0530

    node.next

--- NewBeg/Starter/src/LinkedList.java
+++ NewBeg/Starter/src/LinkedList.java
@@ -142,4 +142,4 @@ public class LinkedList{
             this.next = next;
         }
     }
-}
+} 
---
+++ NewBeg/Starter/src/Twins.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Twins {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        int arr1[] = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int i = 0; i<n; i++){
+            arr1[i] = in.nextInt();
+        }
+        int sum = 0; 
+        int sum1 = 0;
+        for(int i = 0; i<n; i++){
+            sum = sum + arr[i];
+            sum1 = sum1 + arr1[i];
+        }
+
+        int temp = 0;
+        int temp1 = 0; 
+        for(int i = 0; i<n-1; i++){
+            if(arr[i] + arr[i+1] > arr1[i] + arr1[i+1]){
+                
+            }
+
+        }
+}
