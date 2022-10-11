9092c6370f57f17a58f0d634d2ace626713f41cc
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2022-10-11 23:47:42 +0530

    doubley

--- NewBeg/Starter/src/Main.java
+++ NewBeg/Starter/src/Main.java
@@ -4,11 +4,18 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        NewLl list = new NewLl();
+        // NewLl list = new NewLl();
+        // list.insertFirst(23);
+        // list.insertFirst(21);
+        // list.insertFirst(55);
+        // list.display();
+        DoublyLl list = new DoublyLl();
         list.insertFirst(23);
         list.insertFirst(21);
         list.insertFirst(55);
-        list.display();
-        
+        list.insertLast(99);
+
+        //list.display();
+        list.displayRev();
     }
 }
